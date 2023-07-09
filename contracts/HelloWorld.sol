ac2ffa437b293c6c6db58ea45aff7199583e3396
Aakarshit <aakarshit.agarwal@gmail.com>
2023-07-09 21:37:50 +0530

    Creating first contract

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    string private message = "Hello World";
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
